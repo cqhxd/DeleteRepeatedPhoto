84d98106917e9b1d09a163c7057104acfb1250fb
HuangXiaoDong <4097036@qq.com>
2022-11-29 19:44:35 +0800

    2022-11-29-1

---
+++ 删除重复图片/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace 删除重复图片
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 54);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(684, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "D:\\testtmp";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_start.Location = new System.Drawing.Point(547, 88);
+            this.btn_start.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(149, 37);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "开始处理";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label1.Location = new System.Drawing.Point(297, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "正在处理中....";
+            this.label1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.ForeColor = System.Drawing.Color.Gray;
+            this.textBox2.Location = new System.Drawing.Point(12, 95);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(360, 62);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "注：拍摄时间 和 文件字节数 相同 而图片内容不一样的几率几乎为 0，如果再严谨点，加上图片的GPS信息或其它信息联合判断。处理完成后，重复的文件会在当前下目录的" +
+    "fordel目录中。";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "9999";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(350, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "/";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(360, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "9999";
+            this.label4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 166);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "删除重复照片(根据：图片文件的字节数、拍摄时间 判断重复)";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 删除重复图片/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Threading;
+// NuGet包地址---https://www.nuget.org/packages/WindowsAPICodePack-Shell/
+// WindowsAPICodePack-Shell  NuGet包，可以获取照片的所有信息。
+using Microsoft.WindowsAPICodePack.Shell;
+
+// ************************************************************
+// 删除重复的照片
+// 功能描述：清理一个文件夹下重复的照片，把重复的放在该文件夹的 fordel 目录下， 
+// 对比描述：根据照片文件的字节数、拍摄日期对比是否重复
+// 代     码：小黄 2022.11.29   wechat:18983625525 
+// ************************************************************
+namespace 删除重复图片
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = folderBrowserDialog1.SelectedPath;//获取选中文件路径
+            }
+        }
+
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "") { return; }
+            if (!Directory.Exists(textBox1.Text)) { return; }
+
+            this.btn_start.Enabled = false;
+            this.label1.Visible = true; this.label2.Visible = true; this.label2.Text = "";
+            this.label3.Visible = true; this.label4.Visible = true; this.label4.Text = "";
+
+            Thread t = new Thread(do_it);
+            t.Start();
+        }
+
+        //开始
+        private void do_it()
+        {
+            if (!Directory.Exists(textBox1.Text + "\\fordel"))
+            {
+                Directory.CreateDirectory(textBox1.Text + "\\fordel");
+            }
+            var For_del_path = textBox1.Text + "\\fordel"; // 待删除的目录
+
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+
+            FileInfo[] files = dir.GetFiles();  // 1、找出本目录下所有文件 
+
+            Dictionary<string, FileInfo> Dict = new Dictionary<string, FileInfo>();
+
+            for (var i = 0; i < files.Length; i++)
+            {
+                // key 用 ‘文件长度’ + ‘拍摄时间’ 作为Key
+                // 拍摄时间 和 文件字节数 相同的判定为相同文件
+                // 注：拍摄时间 和 文件字节数 相同 而图片内容不一样的几率几乎为 0，如果再严谨点，加上
+                //      图片的GPS信息或其它信息联合判断Key。
+                var key = files[i].Length.ToString() + "_" + GetMediaTimeLen(files[i].FullName);
+                try
+                {
+                    Dict.Add(key, files[i]);
+                }
+                catch { continue; }
+            }
+            //===========================================
+
+            List<FileInfo> tmp_objs = null;
+            FileInfo[] files2 = null;
+
+            SetProcess St = new SetProcess(SetProcessSub);
+
+            for (var i = 0; i < Dict.Count; i++)
+            {
+                string key_str = Dict.Keys.ToArray()[i]; //根据Dictionary的index取Key值
+                string[] tmp_arr = key_str.Split('_');
+
+                FileInfo fi = Dict.Values.ToArray()[i];    //根据Dictionary的index取value值
+                long t_long = Convert.ToInt64(tmp_arr[0].ToString()); //字节长度
+                string t_ps_data = tmp_arr[1].ToString(); //拍摄时间字符串
+
+                this.BeginInvoke(St, new object[] { i, Dict.Count });
+
+                tmp_objs = null;
+                tmp_objs = new List<FileInfo>();
+                files2 = null;
+                files2 = dir.GetFiles();  // 1、找出本目录下所有文件 
+
+                for (var k = 0; k < files2.Length; k++)
+                {
+                    if (files2[k].Length == t_long) //先判断 length ,对了，在判断拍摄时间，不能用 if (files2[k].Length == t_long && time == t_ps_data)，太慢
+                    {
+                        var time = GetMediaTimeLen(files2[k].FullName);//这里来得到文件的拍摄时间
+                        if (time == t_ps_data)
+                        {
+                            tmp_objs.Add(files2[k]);
+                        }
+                    }
+                }
+                //处理tmp_objs
+                //保留 fi ,把其他的移动到 fordel 文件夹
+                if (tmp_objs.Count > 1)
+                {
+                    for (var x = 0; x < tmp_objs.Count; x++)
+                    {
+                        if (tmp_objs[x].FullName != fi.FullName)
+                        {
+                            //剪切到 fordel 文件夹
+                            if (File.Exists(tmp_objs[x].FullName))
+                            {
+                                try
+                                {
+                                    File.Move(tmp_objs[x].FullName, For_del_path + "\\" + tmp_objs[x].Name);
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.Message);
+                                }
+                            }
+                        }
+                    }
+                    //fordel 完成
+                }
+
+            }
+
+            Complete CC = new Complete(IsComplete);
+            this.BeginInvoke(CC);
+        }
+
+
+
+
+
+        // ===================== 2022-11-29 =====================  
+        // WindowsAPICodePack-Shell  NuGet包，可以获取照片的所有信息。
+        // 光圈、快门、镜头、相机、拍摄时间、分辨率、像素、焦距...等等等等......
+        // 在 Properties.System 属性里面找，共124项属性
+        // ===================== 2022-11-29 =====================  
+        private static string GetMediaTimeLen(string path)
+        {
+            using (ShellObject obt = ShellObject.FromParsingName(path))
+            {
+                var tokendata = obt.Properties.System.ItemDate.Value; //如果无'拍摄日期'，应该是取的'修改日期'
+                return tokendata.ToString();
+            }
+        }
+
+
+
+
+        public delegate void Complete();
+
+        public void IsComplete()
+        {
+            this.label1.Visible = false; this.label2.Visible = false;
+            this.label3.Visible = false; this.label4.Visible = false;
+            this.btn_start.Enabled = true;
+        }
+
+        public delegate void SetProcess(Int64 nowInt, Int64 allInt);
+        public void SetProcessSub(Int64 nowInt, Int64 allInt)
+        {
+            this.label2.Text = nowInt.ToString();
+            this.label4.Text = allInt.ToString();
+        }
+
+
+    }
+}
